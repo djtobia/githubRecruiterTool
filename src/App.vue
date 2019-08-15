ee941f53ea544ec925262deca70043c359f55ead
Dylan Tobia <djtobia@gmail.com>
2019-08-14 21:27:38 -0700

    have it working a little more

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,32 @@
     <h1>Welcome to the Github Recruiter Tool!</h1>
     <h2>What kind of search would you like to do?</h2>
     <div class="row">
-      <div class="col-md-6 text-right">
+      <div class="col-md-6 col-sm-3 text-right">
         <label for="usernameInput">Username: </label>
       </div>
-      <div class="col-md-6 text-left">
-        <input id="usernameInput" type="text" v-model="searchInputs.username">
+      <div class="col-md-6 col-sm-3 text-left">
+        <input id="usernameInput" type="text" v-model="searchInputs.user">
       </div>
       <br>
-      <div class="col-md-6 text-right">
-        <label for="locationInput">Location: </label>
+      <div class="col-md-6 col-sm-3 text-right">
+        <label for="locationInput">Location (can be a college): </label>
       </div>
-      <div class="col-md-6 text-left">
+      <div class="col-md-6 col-sm-3 text-left">
         <input id="locationInput" type="text" v-model="searchInputs.location">
-
+      </div>
+      <br>
+      <div class="col-md-6 col-sm-3 text-right">
+          <label for="fullnameInput">Fullname (if you know it!): </label>
+      </div>
+      <div class="col-md-6 col-sm-3 text-left">
+        <input id="fullnameInput" type="text" v-model="searchInputs.fullname">
       </div>
       <div class="col align-self-center">
       <button class="btn btn-primary" @click='search'>Search!</button>
       </div>   
     </div>
-    <users v-if="Object.keys(searchResults).length !== 0"/>
+    <h3 v-if="this.pages !== 0">There are {{ pages }} pages of data for that search result!</h3>
+    <users v-if="searchResults.length !== 0" :usersInfo="searchResults"/>
   </div>
 </template>
 
@@ -32,8 +39,9 @@ export default {
   name: 'app',
   data() {
     return {
-      searchInputs: { username: '', location: ''},
-      searchResults: {}
+      searchInputs: { user: '', location: '', fullname: ''},
+      searchResults: [],
+      pages: 0
     };
   },
   components: {
@@ -41,7 +49,24 @@ export default {
   },
   methods: {
     search() {
-      
+      this.searchResults = [];
+      let query = 'q=';
+      for(const input of Object.keys(this.searchInputs)){
+          if(this.searchInputs[input] !== '')
+            query += input + ':"' + this.searchInputs[input] + '" ';
+      }
+      query = query.trim().replace(/\s/g, '+') +"&per_page=50";
+      axios.get('https://api.github.com/search/users?'+query)
+      .then( response => {
+        console.log(response);
+        if(response.headers.link)
+          this.pages = response.headers.link.split(',')[1].match(/&page=\d*[^>]/g)[0].split('=')[1];
+        
+        for(let item of response.data.items){
+          this.searchResults.push(item);
+        }
+        console.log(this.searchInputs);
+      })
     }
   }
 }
--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,33 +1,55 @@
 <template>
-<div>
-    <table>
-        <thead>
+  <div>
+    <div class="row">
+      <div class="col-md-12">
+        <table>
+          <thead>
+            <th>Avatar</th>
             <th>Username</th>
             <th>URL</th>
-        </thead>
-        <tbody>
+          </thead>
+          <tbody>
             <tr v-for="(user, index) in usersInfo" :key="index">
-                <td>{{ user.login }}</td>
-                <td>{{ user.html_url }}</td>
+              <td>
+                <div class="col-md-2">
+                  <img class="square-img" :src="user.avatar_url" alt="avatar index" />
+                </div>
+              </td>
+              <td>
+                <div class="col-md-3">{{ user.login }}</div>
+              </td>
+              <td>
+                <div class="col-md-5">
+                  <a :href="user.html_url">{{ user.html_url }}</a>
+                </div>
+              </td>
             </tr>
-        </tbody>
-    </table>
-</div>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-    name:'Users',
-    props: {
-        usersInfo: {}
+  name: "Users",
+  props: {
+    usersInfo: {
+      type: Array,
+      default() {
+        return [];
+      }
     }
-}
+  }
+};
 </script>
 
 <style lang="less" scoped>
-.red { 
-    background-color: red;
+.square-img {
+  width: 50px;
+  height: 50px;
 }
 </style>
