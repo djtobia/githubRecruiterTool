e6ab5baabed91917ee9681ef94992470e9671d61
Dylan Tobia <djtobia@gmail.com>
2019-08-19 18:24:35 -0700

    trying again

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,8 @@
-var merge = require('webpack-merge')
 module.exports = {
     configureWebpack: config => {
         if (process.env.NODE_ENV === 'production') {
-                apikey: JSON.stringify(process.env.apikey)
-        } else {
-            // mutate for development...
+            config.apikey = JSON.stringify(process.env.apikey);
         }
+
     }
 }
