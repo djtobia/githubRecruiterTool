52bbfb4c321f62adc4973b014cb9e6e47b28256f
Dylan Tobia <djtobia@gmail.com>
2019-08-20 20:02:54 -0700

    changed the way the user component fetches its data

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -4,7 +4,7 @@
             <div class="col-md-12">
 
                 <v-data-table :headers="tableHeaders"
-                              :items="usersInfoMain"
+                              :items="usersInfo"
                               :disable-sort="true"
                               :server-items-length="totalItems > 1000 ? 1000 : totalItems"
                               :items-per-page="itemsPerPage"
@@ -15,7 +15,6 @@
                                 showFirstLastPage: true
                               }"
                               :show-expand="true"
-
                               @update:options="updateOptions($event)"
                 >
                     <template slot="item.avatar_url" slot-scope="props">
@@ -24,29 +23,16 @@
                     <template slot="item.url" slot-scope="props">
                         <a :href="props.item.url" target="_blank">{{props.item.url}}</a>
                     </template>
-                    <template slot="item.name" slot-scope="props">
-                       <span v-if="usersInfoSecondary[props.item.id]">{{ usersInfoSecondary[props.item.id].fullname }}</span>
-                    </template>
-                    <template slot="item.email" slot-scope="props">
-                       <span v-if="usersInfoSecondary[props.item.id]"> {{ usersInfoSecondary[props.item.id].email }} </span>
-                    </template>
-                    <template slot="item.company" slot-scope="props">
-                        <span v-if="usersInfoSecondary[props.item.id]"> {{ usersInfoSecondary[props.item.id].company }}</span>
-                    </template>
-                    <template slot="item.location" slot-scope="props">
-                        <span v-if="usersInfoSecondary[props.item.id]">{{ usersInfoSecondary[props.item.id].location }}</span>
-                    </template>
                     <template slot="item.hireable" slot-scope="props">
-                        <span v-if="usersInfoSecondary[props.item.id]">
-                            <v-chip :color="getColor(usersInfoSecondary[props.item.id].hireable)" dark>{{usersInfoSecondary[props.item.id].hireable}}</v-chip>
-                        </span>
+                        <v-chip :color="getColor(props.item.hireable)" dark>{{props.item.hireable}}</v-chip>
+
                     </template>
                     <template slot="expanded-item" slot-scope="props">
-
-                        <td :colspan="tableHeaders.length-2">Bio: <span v-if="usersInfoSecondary[props.item.id]">{{ usersInfoSecondary[props.item.id].bio }}</span></td>
+                        <td :colspan="tableHeaders.length-2">Bio: {{ props.item.bio }}</td>
                         <td :colspan="2">Website :
-                            <span v-if="usersInfoSecondary[props.item.id] && usersInfoSecondary[props.item.id].website !== 'None Listed'"><a :href="usersInfoSecondary[props.item.id].website" target="_blank">{{usersInfoSecondary[props.item.id].website}}</a></span>
-                        <span v-else>None Listed</span></td>
+                            <span v-if="props.item.website !== 'None Listed'"><a
+                                    :href="props.item.website" target="_blank">{{ props.item.website }}</a></span>
+                            <span v-else>None Listed</span></td>
                     </template>
                 </v-data-table>
             </div>
@@ -84,7 +70,7 @@
                     {text: 'Location', value: 'location'},
                     {text: 'Hireable', value: 'hireable'}
                 ],
-                usersInfoMain: [],
+                usersInfo: [],
                 usersInfoSecondary: [],
                 pages: 0,
                 totalItems: 0,
@@ -100,13 +86,13 @@
         },
         methods: {
             fetchSearchResults() {
-                this.usersInfoMain.splice(0, this.usersInfoMain.length);
+                this.usersInfo.splice(0, this.usersInfo.length);
                 this.loading = true;
                 if (this.query !== '') {
                     if (!this.query.match('&per_page=')) {
                         this.query = this.query + '&per_page=' + this.itemsPerPage;
                     }
-                    axios.get('https://api.github.com/search/users?' + this.query,{
+                    axios.get('https://api.github.com/search/users?' + this.query, {
                         auth: {
                             username: 'djtobia',
                             password: process.env.VUE_APP_API_KEY
@@ -118,50 +104,37 @@
                                 this.pages = response.headers.link.split(',')[1].match(/&page=\d*[^>]/g)[0].split('=')[1];
                             let index = 0;
                             for (let item of response.data.items) {
-                                this.usersInfoMain.push(this.formatItem(item, index++));
+                                axios.get('https://api.github.com/users/' + item.login, {
+                                    auth: {
+                                        username: 'djtobia',
+                                        password: process.env.VUE_APP_API_KEY
+                                    },
+                                }).then(response2 => {
+                                    this.usersInfo.push(this.formatItem(item, response2.data, index++));
+                                })
                             }
                             this.loading = false;
-                            this.getSecondaryInfo();
                         });
                 }
             },
-            getSecondaryInfo() {
-                //for each login in usersInfoMain, get that user and store in secondary.
-                this.usersInfoSecondary.splice(0, this.usersInfoSecondary.length);
-                for (let user of this.usersInfoMain) {
-                    axios.get('https://api.github.com/users/' + user.username, {
-                        auth: {
-                            username: 'djtobia',
-                            password: process.env.VUE_APP_API_KEY
-                        }
-                    }).then(response => {
-                        this.usersInfoSecondary.push(this.formatSecondaryData(response.data));
-                    })
-                }
-            },
-            formatItem(item, index) {
-                let newItem = {};
-                newItem['id'] = index;
-                newItem['avatar_url'] = item.avatar_url;
-                newItem['username'] = item.login;
-                newItem['url'] = item.html_url;
-                return newItem;
-            },
-            formatSecondaryData(data) {
-                let website = data.blog;
-                if(website && website.search('http') === -1){
+            formatItem(searchInfo, userInfo, index) {
+                let website = userInfo.blog;
+                if (website && website.search('http') === -1) {
                     website = 'http://' + website;
                 }
-                let formattedData = {
-                    bio: data.bio ? data.bio : 'None Listed',
-                    website: website ? website : 'None Listed',
-                    company: data.company ? data.company : 'None Listed',
-                    email: data.email ? data.email : 'None Listed',
-                    location: data.location ? data.location : 'None Listed',
-                    fullname: data.name ? data.name : 'No Name Listed',
-                    hireable: data.hireable ? 'Yes' : 'No'
-                };
-                return formattedData;
+                let newItem = {};
+                newItem['id'] = index;
+                newItem['avatar_url'] = searchInfo.avatar_url;
+                newItem['username'] = searchInfo.login;
+                newItem['url'] = searchInfo.html_url;
+                newItem['website'] = website ? website : 'None Listed';
+                newItem['bio'] = userInfo.bio ? userInfo.bio : 'None Listed';
+                newItem['location'] = userInfo.location ? userInfo.location : 'None Listed';
+                newItem['company'] = userInfo.company ? userInfo.company : 'None Listed';
+                newItem['email'] = userInfo.email ? userInfo.email : 'None Listed';
+                newItem['name'] = userInfo.name ? userInfo.name : 'None Listed';
+                newItem['hireable'] = userInfo.hireable ? 'Yes' : 'No';
+                return newItem;
             },
             updateOptions(newOptions) {
                 this.itemsPerPage = newOptions.itemsPerPage;
